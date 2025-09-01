751b04ee8aeab5eba83171891b1be75078c50f6c
An Isle <ljygeek@gmail.com>
2025-09-01 08:44:52 -0700

    Update overview.py

--- aerialview/app/overview.py
+++ aerialview/app/overview.py
@@ -1,22 +1,37 @@
-from dash import html, dcc
+from dash import html, dcc, Input, Output
 import plotly.graph_objs as go
-from aerialview.utils.visualizations import create_candlestick_chart
 from aerialview.utils.data_fetch import fetch_stock_data
+from aerialview.core.visualize import multi_ticker_comparison
+from aerialview.app import app
 
-default_ticker = "AAPL"
-stock_data = fetch_stock_data(default_ticker)
-candlestick_chart = create_candlestick_chart(stock_data)
+default_tickers = ["AAPL", "MSFT", "TSLA", "GOOGL"]
 
 layout = html.Div([
     html.H1("Overview Page"),
     html.P("This page provides an overview of the application metrics."),
+    dcc.Dropdown(
+        id='ticker-dropdown',
+        options=[{'label': ticker, 'value': ticker} for ticker in default_tickers],
+        value=default_tickers,
+        multi=True
+    ),
     html.Div(id='overview-charts-placeholder', children=[
         dcc.Loading(
             type="default",
             children=dcc.Graph(
-                figure=candlestick_chart,
-                id='candlestick-chart'
+                id='comparison-chart'
             )
         )
     ])
 ])
+
+@app.callback(
+    Output('comparison-chart', 'figure'),
+    Input('ticker-dropdown', 'value')
+)
+def update_comparison_chart(selected_tickers):
+    if not selected_tickers:
+        selected_tickers = default_tickers
+    data = {ticker: fetch_stock_data(ticker) for ticker in selected_tickers}
+    fig = multi_ticker_comparison(data)
+    return fig
