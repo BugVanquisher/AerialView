6fc419bc18aa3908ad2ddfa9af529f545ffc0435
An Isle <ljygeek@gmail.com>
2025-08-31 23:29:07 -0700

    refactor index.py

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output
 import yfinance as yf
+from dash import Dash
 
-from app import app
 from pages import stock_overview, overview
 
+app = Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -16,11 +17,14 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/aerial-view/stock-overview':
+    if pathname == '/stock-overview':
         return stock_overview.layout
+    elif pathname == '/overview':
+        return overview.layout
     else:
         return overview.layout
 
 if __name__ == '__main__':
+    # Override pandas datareader with yfinance to fix Yahoo Finance API issues
     yf.pdr_override()
     app.run_server(debug=True)
